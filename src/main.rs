637e7ccc3fabdc78f50b54052fddc1bc236f478b
Mynacol <Mynacol@noreply.codeberg.org>
2024-05-05 18:12:01 +0200

    Replace the cmdline interaction
    Compare by default `/run/booted-system` with `/run/current-system`.
    An optional path can be given as cmdline argument, which replaces
    `/run/current-system` with the given path. That allows to test a
    required reboot for built, but not applied system configurations (e.g.
    during dry-activation).
    
    No marker file is written, instead a clear exit code is used. Exit code
    0 stands for "reboot needed", while exit code 2 stands for explicitly
    "no reboot needed" (while exit code 1 is preserved for general error
    messages).

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,41 @@
 use std::env;
 use std::error::Error;
 use std::fs;
-use std::io::{self, Write};
 use std::path::Path;
 
 mod compare_nixos_modules;
 
 pub static OLD_SYSTEM_PATH: &str = "/run/booted-system";
-pub static NEW_SYSTEM_PATH: &str = "/nix/var/nix/profiles/system";
-pub static NIXOS_NEEDS_REBOOT: &str = "/var/run/reboot-required";
+pub static NEW_SYSTEM_PATH: &str = "/run/current-system";
 
 fn main() -> Result<(), Box<dyn Error>> {
     let env_args: Vec<String> = env::args().collect();
-    let dry_run = env_args.contains(&String::from("--dry-run"));
 
     if env_args.contains(&String::from("--version")) {
         println!("{}: v{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
         std::process::exit(0);
     }
 
-    let user = env::var_os("USER")
-        .unwrap()
-        .into_string()
-        .expect("Cannot convert OsString into String");
-    if user != "root" && !dry_run {
-        println!("ERROR: please run this as root");
-        println!("HINT: use the '--dry-run' option");
-        std::process::exit(1);
-    }
+    let new_system_path = if env_args.len() == 2 { &env_args[1] } else { NEW_SYSTEM_PATH };
 
     if Path::new("/nix/var/nix/profiles/system").exists() {
         let old_system_id = fs::read_to_string(OLD_SYSTEM_PATH.to_string() + "/nixos-version")?;
-        let new_system_id = fs::read_to_string(NEW_SYSTEM_PATH.to_string() + "/nixos-version")?;
+        let new_system_id = fs::read_to_string(new_system_path.to_string() + "/nixos-version")?;
 
-        if Path::new(NIXOS_NEEDS_REBOOT).exists() {
-            let stdout = io::stdout();
-            let mut handle = stdout.lock();
-            let _ = handle.write_all(&fs::read(NIXOS_NEEDS_REBOOT)?);
-            let _ = handle.flush();
-        } else if old_system_id == new_system_id {
+        if old_system_id == new_system_id {
             eprintln!("DEBUG: you are using the latest NixOS generation, no need to reboot");
+            std::process::exit(2);
         } else {
             let reason = compare_nixos_modules::upgrades_available()?;
             if reason.is_empty() {
                 eprintln!("DEBUG: no updates available, moar uptime!!!");
+                std::process::exit(2);
             } else {
-                if dry_run {
-                    println!("{reason}");
-                } else {
-                    fs::write(NIXOS_NEEDS_REBOOT, reason)?;
-                }
+                println!("{reason}");
             }
         }
     } else {
-        eprintln!("This binary is intedned to run only on NixOS.");
+        eprintln!("This binary is intended to run only on NixOS.");
         std::process::exit(1);
     }
 
